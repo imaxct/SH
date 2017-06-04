7b12ce21de7dfcb339fbf90798de65866c42c7d4
imaxct <imaxct@foxmail.com>
2017-06-04 21:23:27 +0800

    update.

--- exercises/ex_multi-line_read.sh
+++ exercises/ex_multi-line_read.sh
@@ -2,3 +2,15 @@
 while read line
 do echo "$line ${#line}"
 done < inputfile
+
+OIFS=$IFS; IFS=:
+while read name passwd uid gid fullname ignore
+do echo "$name ($fullname)"
+done < /etc/passwd
+IFS=$OIFS
+
+while IFS=: read name passwd uid gid fullname ignore
+do echo "$name ($fullname) ${#name}"
+done < /etc/passwd
+
+echo "IFS is $IFS"
